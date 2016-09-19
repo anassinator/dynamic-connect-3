223fb3f28ceb94a66753a63be9892e22ab087a78
Anass Al <mr@anassinator.com>
2016-09-18 20:26:17 -0400

    Add Game definition

---
+++ game.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from board import SmallBoard, Player
+from move import Move, Direction, InvalidMove
+
+class Game(object):
+
+    """A game instance.
+    
+    Attributes:
+        board: Current board state.
+        turn: Current player.
+    """
+
+    def __init__(self, board):
+        """Constructs a Game instance from a given starting position.
+        
+        Args:
+            board: Starting board position.
+        """
+        self.board = board
+        self.turn = Player.white
+
+    def play(self, move):
+        """Plays a given move and switches to next player's turn.
+        
+        Args:
+            move: Move to play.
+        """
+        if self.board.get(move.x, move.y) == 1 - self.turn.value:
+            raise InvalidMove("Not your turn")
+
+        self.board.move(move)
+        self.turn = Player(not self.turn.value)
+
+
+if __name__ == "__main__":
+    board = SmallBoard()
+    game = Game(board)
+    print(game.board)
+
+    while True:
+        try:
+            s = "{}'s turn. Enter a move: ".format(game.turn.name.capitalize())
+            move = Move.from_str(input(s))
+            game.play(move)
+            print(game.board)
+        except InvalidMove as e:
+            print(e.message)
+        except (KeyboardInterrupt, EOFError):
+            break
