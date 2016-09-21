46774f8f1cfd0dbc6ff1ee28be7785c65fe9798e
Anass Al <mr@anassinator.com>
2016-09-21 01:30:45 -0400

    Add WeightedHeuristic and GoalHeuristic

--- heuristics.py
+++ heuristics.py
@@ -22,16 +22,52 @@ class Heuristic(object, metaclass=ABCMeta):
             The estimated value of the board such that the more positive it is
             the more in favor of the white player the board is and the more
             negative it is, the more in favor of the black player the board is.
+        """
+        raise NotImplementedError
 
+
+class WeightedHeuristic(object):
+
+    """A heuristic and corresponding weight pairing.
+    
+    Attributes:
+        heuristic: Heuristic.
+        weight: Correspoding weight.
+    """
+
+    def __init__(self, heuristic: Heuristic, weight: float):
+        """Constructs a WeightedHeuristic.
+
+        Args:
+            heuristic: Heuristic.
+            weight: Corresponding weight.
+        """
+        self.heuristic = heuristic
+        self.weight = weight
+
+
+class GoalHeuristic(Heuristic):
+
+    """A heuristic based on whether a player has won the game or not."""
+
+    @classmethod
+    def compute(cls, board: Board, player: Player) -> float:
+        """Computes the heuristic's value for a given game state.
+
+        Args:
+            board: Current board.
+            player: Current player.
+
+        Returns:
             A white win is represented as +inf and a black win is represented
-            as -inf.
+            as -inf. Anything else returns 0.
         """
         if board.is_goal(Player.white):
             return float("inf")
         elif board.is_goal(Player.black):
             return float("-inf")
-        else:
-            raise NotImplementedError
+
+        return 0
 
 
 class NumberOfRunsOfTwoHeuristic(Heuristic):
@@ -52,18 +88,8 @@ class NumberOfRunsOfTwoHeuristic(Heuristic):
             player: Current player.
 
         Returns:
-            The estimated value of the board such that the more positive it is
-            the more in favor of the white player the board is and the more
-            negative it is, the more in favor of the black player the board is.
-
-            A white win is represented as +inf and a black win is represented
-            as -inf.
+            The number of runs of 2 the player is leading the black player by.
         """
-        try:
-            return super().compute(board, player)
-        except NotImplementedError:
-            pass
-
         if cls.RUNS_OF_TWO is None:
             board_class = type(board)
             cls.RUNS_OF_TWO = generate_streaking_boards(board_class, 2)
