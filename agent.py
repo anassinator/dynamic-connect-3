702bca657154a9c2b8759efb0402d3aee354bbb4
Anass Al <mr@anassinator.com>
2016-09-22 01:57:58 -0400

    Add preliminary minimax search

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@
 from game import Game
 from move import Move
 from typing import List
+from threading import Lock
 from base_board import Board, Player
 from heuristics import WeightedHeuristic
 
@@ -17,28 +18,35 @@ class Agent(object):
         heuristics: List of weighted heuristics.
     """
 
-    def __init__(self, game: Game, player: Player,
+    def __init__(self, board: Board, player: Player,
                  heuristics: List[WeightedHeuristic]):
         """Constructs an Agent.
 
         Args:
-            game: Game to play.
+            board: Board to play on.
             player: Player to play as.
             heuristics: List of weighted heuristics to use.
         """
-        self.game = game.copy()
+        self.game = Game(board.copy())
         self.player = player
         self.heuristics = heuristics
+        self.__lock = Lock()
 
     def request_move(self) -> Move:
         """Request the best available move so far.
-        
+
         Returns:
             Best available move agent so far.
         """
-        raise NotImplementedError
+        with self.__lock:
+            board = self.game.board
+            turn = self.game.turn
 
-    def state_updated(self, move: Move, turn: Player) -> None:
+        move, value = self._minimax(board, turn, 5)
+
+        return move
+
+    def update_state(self, move: Move, turn: Player) -> None:
         """Notifies the agent that a player has made a move and that the agent
         should observe the new game state.
 
@@ -46,7 +54,8 @@ class Agent(object):
             move: Move played by the player.
             player: Who's turn it is now.
         """
-        raise NotImplementedError
+        with self.__lock:
+            self.game.play(move)
 
     def __compute_heuristic(self, board: Board, player: Player) -> float:
         """Computes the weighted heuristic for the game state given.
@@ -63,7 +72,60 @@ class Agent(object):
             considers.
         """
         heuristic = 0
-        for wh in heuristics:
+        for wh in self.heuristics:
             heuristic += wh.weight * wh.heuristic.compute(board, player)
         return heuristic 
+
+    def __minimax_comparator(self, best_value: float, current_value: float,
+                             turn: Player) -> bool:
+        """Compares heuristic values based on the turn such that each player
+        plays their optimal move.
+
+        The black player tries to minimize the heuristic value, whereas the
+        white player tries to maximize it.
+
+        Args:
+            best_value: Current best value.
+            current_value: Current heuristic value to evaluate.
+            turn: Current player's turn.
+
+        Return:
+            Whether the current value is better or not.
+        """
+        if best_value is None:
+            return True
+        if turn == Player.white:
+            return current_value > best_value
+        elif turn == Player.black:
+            return current_value < best_value
+        else:
+            raise NotImplementedError
+
+    def _minimax(self, board: Board, turn: Player, max_depth: int):
+        """Selects the best move given the current board state by looking up to
+        a certain depth.
+
+        Args:
+            board: Current root board state.
+            turn: Current player's turn.
+            max_depth: Max depth to look at.
+
+        Returns:
+            Tuple of (best move, heuristic value).
+        """
+        next_turn = Player.black if turn == Player.white else Player.white
+        if max_depth == 0 or board.is_goal(next_turn):
+            return (None, self.__compute_heuristic(board, turn))
+
+        best_move = None
+        best_value = None
+        for move in board.available_moves(turn):
+            child_board = board.copy()
+            child_board.move(move)
+            _, v = self._minimax(child_board, next_turn, max_depth - 1)
+            if self.__minimax_comparator(best_value, v, turn):
+                best_value = v
+                best_move = move
+
+        return (best_move, best_value)
 
